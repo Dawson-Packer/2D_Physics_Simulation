f920018a902502fe7900248a3aa6d7df1991053e
Dawson Packer <125329638+Dawson-Packer@users.noreply.github.com>
2023-11-02 22:13:50 -0800

    Simplified median and fixed BVH to corrected Smart Space Partitioning

--- CollisionDetection.py
+++ CollisionDetection.py
@@ -1,23 +1,17 @@
-import numpy as np
 from objects.objects import *
 
-def idx_median(lst):
-    if len(lst) % 2 == 1:
-        return np.where(lst == np.median(lst))[0][0]
-    else:
-        length, remainder = len(lst) // 2 - 1, len(lst) // 2
-        left = np.partition(lst, length)[length]
-        right = np.partition(lst, remainder)[remainder]
-        return [np.where(lst == left)[0][0], np.where(lst == right)[0][0]]
+def median(lst):
+    return len(lst) // 2
 
 def handle_collisions(objects: list):
+    pass
+
+def calculate_ssp(objects: list):
     objects_flattened_x = sorted(objects, key=lambda x: x.x_pos)
     objects_x_coords = [object.x_pos for object in objects_flattened_x]
 
     # First split
-    split_idx = idx_median(objects_x_coords)
-    if type(split_idx) is list:
-        split_idx = split_idx[1]
+    split_idx = median(objects_x_coords)
     objects_flattened_x = [objects_flattened_x[0:split_idx], objects_flattened_x[split_idx+1:]]
 
     for split in objects_flattened_x:
@@ -26,13 +20,8 @@ def handle_collisions(objects: list):
             objects_y_coords = [object.y_pos for object in objects_flattened_y]
 
             # Second split
-            split_idx = idx_median(objects_y_coords)
-            if type(split_idx) is list:
-                split_idx = split_idx[1]
+            split_idx = median(objects_y_coords)
             objects_flattened_y = [objects_flattened_y[0:split_idx], 
-                                   objects_flattened_y[split_idx+1:]]
-            
-def calculate_bvh(objects: list):
-    pass
+                                    objects_flattened_y[split_idx:]]
 
     
