00f25f58a3b2d1ed562a906047e1d60c14cf19c8
Dawson Packer <125329638+Dawson-Packer@users.noreply.github.com>
2023-11-02 22:14:36 -0800

    Updated CollisionDetection

--- CollisionDetection.py
+++ CollisionDetection.py
@@ -1,8 +1,5 @@
 from objects.objects import *
 
-def median(lst):
-    return len(lst) // 2
-
 def handle_collisions(objects: list):
     pass
 
@@ -11,7 +8,7 @@ def calculate_ssp(objects: list):
     objects_x_coords = [object.x_pos for object in objects_flattened_x]
 
     # First split
-    split_idx = median(objects_x_coords)
+    split_idx = len(objects_x_coords) // 2
     objects_flattened_x = [objects_flattened_x[0:split_idx], objects_flattened_x[split_idx+1:]]
 
     for split in objects_flattened_x:
@@ -20,7 +17,7 @@ def calculate_ssp(objects: list):
             objects_y_coords = [object.y_pos for object in objects_flattened_y]
 
             # Second split
-            split_idx = median(objects_y_coords)
+            split_idx = len(objects_y_coords) // 2
             objects_flattened_y = [objects_flattened_y[0:split_idx], 
                                     objects_flattened_y[split_idx:]]
 
